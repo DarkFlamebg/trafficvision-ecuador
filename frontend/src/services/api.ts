695678d1c601db4753fbdc78d85292b4eb45c9d6
Steven Bazan <ste_alex_bazan@hotmail.com>
2026-08-11 16:29:37 -0500

    FIX APP FRONT

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getBaseUrl = () => {
-  return import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+  return import.meta.env.VITE_API_URL || 'http://26.124.86.170:8000';
 }
 
 export const getWsUrl = () => {
